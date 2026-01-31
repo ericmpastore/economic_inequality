10906804a04bbf449b7d49dc6e8817922018f309
Eric P <epas001@hotmail.com>
2026-01-31 08:22:07 -0500

    Added functionality to show plot with 0 axis

--- inequality.py
+++ inequality.py
@@ -7,15 +7,29 @@ def render_plot(csv_file='GINIUSA.csv'):
     """
     Docstring for render_plot
 
-    EPastore 01/23/26, Render plot reads data from csv file, parses, and shows plot
-
-    EPastore 01/24/26, Removed axis labels, labeled data points, added comments
+    EPastore 01/30/26, Render plot reads data from csv file, parses, and shows plot
     
     :param csv_file: Parameter identifies file to read source data
     """
-    # read csv parameter into dataframe, encoded as Windows 1252 due to error reading original csv, EPastore 01/24/26
-    connections_count = pd.read_csv(csv_file)
-    print(connections_count.head())
+    # read csv parameter into dataframe, EPastore 01/30/26
+    gini_frame = pd.read_csv(csv_file)
+
+    # convert dates to full dates, EPastore 01/31/26
+    gini_frame['observation_date'] = pd.to_datetime(gini_frame['observation_date'])
+
+    x = gini_frame['observation_date']
+    y = gini_frame['SIPOVGINIUSA']
+
+    plt.plot(x,y)
+    plt.ylim(bottom=0)
+    plt.tight_layout()
+    # for i in range(len(x)):
+    #     plt.annotate(str(int(y.iloc[i])), xy=(x.iloc[i], y.iloc[i]), 
+    #              xytext=(0, 5), textcoords='offset points', 
+    #              ha='center', fontsize=9)
+    plt.show()
+    
+
 
 def main():
     render_plot()
